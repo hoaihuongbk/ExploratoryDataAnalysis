d49ecbe0052677e5d0f80a0f9d9d45db688792c7
Hoai Huong <huongv@now.vn>
2019-04-30 23:00:16 +0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,15 @@
-pm25 <- read.table("./summarySCC_PM25.rds")
-str(pm25)
-head(pm25)
+# Load data
+source("./getData.R")
+
+# Initial PNG file
+png(filename = "plot1.png")
+par(mar=c(5, 5, 5, 5))
+
+# Summary
+NEI_1 <- with(NEI, tapply(Emissions, year, FUN = sum))
+
+# Create plot
+barplot(NEI_1, xlab = "Year", ylab = "Total Emissions (tons)", main="Total emissions from PM2.5 in the United States")
+
+# Close file
+dev.off()
